9cee6c55ebcc502c9420b36b6c740e05fd181a73
Vlad <vlad9864gr@gmail.com>
2021-07-27 18:11:26 +0300

    SecondCardDone

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -41,6 +41,16 @@ module.exports = {
         ],
         dsl: 'less'
     },
+    elogic: {
+        area: 'frontend',
+        name: 'Magento/elogic_theme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     backend: {
         area: 'adminhtml',
         name: 'Magento/backend',
